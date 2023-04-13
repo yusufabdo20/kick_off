051104d626bd15f6b88195dea49df09d5709490f
yusufabdo20 <yusufa.fattah@gmail.com>
2023-04-13 06:24:30 +0200

    Yusuf Abdo changes

--- lib/screens/admin_screens/ImagePicker.dart
+++ lib/screens/admin_screens/ImagePicker.dart
@@ -25,19 +25,19 @@ class _ImagePickerScreenState extends State<ImagePickerScreen> {
     });
   }
 
-  Future uploadImage() async {
-    var request = http.MultipartRequest('POST', Uri.parse('YOUR_API_ENDPOINT'));
+  // Future uploadImage() async {
+  //   var request = http.MultipartRequest('POST', Uri.parse('YOUR_API_ENDPOINT'));
 
-    request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
+  //   request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
 
-    var response = await request.send();
+  //   var response = await request.send();
 
-    if (response.statusCode == 200) {
-      print('Image uploaded successfully');
-    } else {
-      print('Image upload failed');
-    }
-  }
+  //   if (response.statusCode == 200) {
+  //     print('Image uploaded successfully');
+  //   } else {
+  //     print('Image upload failed');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
